5ef26e08f26f55b0ea36a517d2c7d0e24361344b
oi-io <boss@oi-io.cc>
2026-07-12 09:53:00 +0800

    refactor(scripts): 整理脚本文件结构，将脚本统一移入scripts目录
    将根目录下的零散脚本文件移动到scripts子目录，包括snmpv3测试、git初始化、打包、发布检查等脚本，删除重复冗余的根目录脚本文件，同时移除无用的layout-audit.js脚本

--- git_init.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-git init
-git config --global user.email boss@oi-io.cc
-git config --global user.name oi-io0
-git remote add gitee git@gitee.com:oi-io0/ipma.git
-git branch -m master
-echo target/ > .gitignore
