28ed11012fe6197c42d7c2eedb8420c8760872cf
oi-io <boss@oi-io.cc>
2026-07-17 21:26:36 +0800

    chore: bump version to 0.11.9 and fix device modal layout
    1. 调整设备模态框中表单控件的布局顺序，将序列号输入框和保存为模板选项的位置复原
    2. 修正保存为模板复选框和标签的排版样式
    3. 更新各静态资源文件的版本号匹配新版本

--- web/static/js/utils/resourceLoader.js
+++ web/static/js/utils/resourceLoader.js
@@ -2,7 +2,7 @@ const moduleCache = new Map();
 const loadingPromises = new Map();
 const preloadedModules = new Set();
 
-export const MODULE_VERSION = '01108';
+export const MODULE_VERSION = '01109';
 
 function withVersion(path) {
     if (!path) return path;
